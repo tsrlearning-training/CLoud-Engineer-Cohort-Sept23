2726c3a1ca3bb05d1fb78d699f3168b0b4ebec76
tsrlearning <info@tsrlearning.io>
2024-01-21 18:08:48 -0600

    Iac deploy

---
+++ Terraform/Azure-resources/locals.tf
@@ -0,0 +1,14 @@
+locals {
+  common_tags = {
+    CompanyName = "TSR Learning"
+    CohortBatch = "Cloud Engineering"
+    Provider    = "Azure Cloud"
+    ManagedWith = "Terraform"
+    casecode    = "tsr2024"
+  }
+  application_tags = {
+
+  }
+  network_tags  = {}
+  security_tags = {}
+}
---
+++ Terraform/Azure-resources/network.tf
@@ -0,0 +1,33 @@
+resource "azurerm_public_ip" "pip" {
+  name                = "pip-tsrlearning-dev-01"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-tsrlearning-dev-01"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "snet" {
+  name                 = "snet-tsrlearning-dev-01"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-tsrlearning-dev-01"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "devnicsnet"
+    subnet_id                     = azurerm_subnet.snet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
