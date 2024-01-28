b51028bf999d5014d11d38fa003d984c98df3875
tsrlearning <info@tsrlearning.io>
2024-01-28 17:40:29 -0600

    login form and infra. code

--- Terraform/Azure-resources/output.tf
+++ Terraform/Azure-resources/output.tf
@@ -1,7 +1,3 @@
 output "vm_public_ip" {
   value = azurerm_linux_virtual_machine.vm.public_ip_address
 }
-
-output "lookupvm_public_ip" {
-  value = azurerm_linux_virtual_machine.lookup_vm.public_ip_address
-}
---
+++ Terraform/Azure-resources/prod/backend.hcl
@@ -0,0 +1,4 @@
+resource_group_name  = "rg-tsrlearning-state"
+storage_account_name = "tsrlearningstor"
+container_name       = "terraformstate"
+key                  = "prod.terraform.tfstate"
