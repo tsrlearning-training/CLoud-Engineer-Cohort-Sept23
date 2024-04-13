edd6da8cbf922e0e00f9c1336957dd9f200cfde0
tsrlearning <info@tsrlearning.io>
2024-04-13 14:05:19 -0500

    nginx web app

--- Docker/infrastructure/rules.tf
+++ Docker/infrastructure/rules.tf
@@ -42,3 +42,18 @@ resource "azurerm_network_security_rule" "https_rule" {
   resource_group_name         = module.resource_group.rg_name
   network_security_group_name = module.subnet.nsg_name
 }
+
+resource "azurerm_network_security_rule" "php_rule" {
+  name                        = "PHPRule"
+  priority                    = 103
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "9000"
+  source_address_prefix       = "143.55.59.117/32"
+  destination_address_prefix  = "*"
+  description                 = "NSG used for PHP connectivity"
+  resource_group_name         = module.resource_group.rg_name
+  network_security_group_name = module.subnet.nsg_name
+}
