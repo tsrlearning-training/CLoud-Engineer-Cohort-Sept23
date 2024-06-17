673a2e8602a863f09fea02ba9311e805fa6ba2dc
tsrlearning <info@tsrlearning.io>
2024-06-17 01:50:03 -0500

    terraform state drift monitoring

--- Docker/infrastructure/scripts/check_drift.sh
+++ Docker/infrastructure/scripts/check_drift.sh
@@ -5,15 +5,16 @@ AZURE_CLIENT_ID=$1
 AZURE_CLIENT_SECRET=$2
 AZURE_TENANT_ID=$3
 SLACK_WEBHOOK_URL=$4
+DIR=$5
 
 # Log in to Azure using service principal
 az login --service-principal -u "$AZURE_CLIENT_ID" -p "$AZURE_CLIENT_SECRET" --tenant "$AZURE_TENANT_ID"
 
 # Initialize Terraform
-terraform init
+cd $DIR && terraform init
 
 # Run Terraform Plan
-terraform plan -detailed-exitcode > plan_output.txt
+cd $DIR &&  terraform plan -detailed-exitcode > plan_output.txt
 PLAN_EXIT_CODE=$?
 
 if [ $PLAN_EXIT_CODE -eq 2 ]; then
