ec12d93530c5145152835d86202a6da51ead3d30
tsrlearning <info@tsrlearning.io>
2024-06-23 15:48:14 -0500

    test terraform data template and azure custom data on vms

--- Docker/infrastructure/scripts/ghrunner_install.sh
+++ Docker/infrastructure/scripts/ghrunner_install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 exec > >(sudo tee -a /var/log/ghrunner_install.log) 2>&1
 
+
+# install
+sudo apt  install jq -y
+sudo apt-get install expect -y
+
 # Declare variables
 RUNNER_URL="${RUNNER_URL}"
 RUNNER_SHA="${RUNNER_SHA}"
@@ -27,9 +32,38 @@ tar xzf "${RUNNER_TAR}"
 
 curl -L  -X POST -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer ${TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" \
-    https://api.github.com/orgs/ORG/actions/runners/registration-token > reponse.json 
+    https://api.github.com/orgs/ORG/actions/runners/registration-token > response.json 
 
 RUNNER_TOKEN=$(jq -r '.token' response.json)
 echo "RUNNER_TOKEN: $RUNNER_TOKEN"
 
-./config.sh --url https://github.com/tsrlearning-training --token $RUNNER_TOKEN
+sudo chown -R tsrlearning:tsrlearning /actions-runner
+# ./config.sh --url https://github.com/tsrlearning-training --token $RUNNER_TOKEN
+
+# Change to the actions-runner directory
+cd /home/tsrlearning/actions-runner
+
+# Start the configuration script
+spawn ./config.sh --url $github_url --token $RUNNER_TOKEN
+
+expect "Enter the name of the runner group to add this runner to:" {
+    send "\r"
+}
+
+expect "Enter the name of runner:" {
+    send "\r"
+}
+
+expect "Enter any additional labels (ex. label-1,label-2):" {
+    send "ghrunner-vm01\r"
+}
+
+expect "Enter name of work folder:" {
+    send "\r"
+}
+
+# Wait for the process to complete
+expect eof
+
+sudo ./svc.sh install
+sudo ./svc.sh start
