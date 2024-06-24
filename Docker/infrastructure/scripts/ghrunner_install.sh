30b0e0d26684c15f6be7cab995279fe0200e1048
tsrlearning <info@tsrlearning.io>
2024-06-23 21:21:06 -0500

    test terraform data template and azure custom data on vms

--- Docker/infrastructure/scripts/ghrunner_install.sh
+++ Docker/infrastructure/scripts/ghrunner_install.sh
@@ -27,7 +27,7 @@ echo "TOKEN:      ${TOKEN}"
 mkdir -p "$HOME/actions-runner"
 cd "$HOME/actions-runner"
 echo $PWD
-sudo chown -R $USER:$USER "$HOME/actions-runner"
+# sudo chown -R $USER:$USER "$HOME/actions-runner"
 
 curl -o actions-runner-linux-x64-2.317.0.tar.gz -L "${RUNNER_URL}"
 echo "${RUNNER_SHA}  actions-runner-linux-x64-2.317.0.tar.gz" | shasum -a 256 -c
@@ -36,6 +36,9 @@ tar xzf "${RUNNER_TAR}"
 curl -L  -X POST -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer ${TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" \
     https://api.github.com/orgs/$GITHUB_ORG/actions/runners/registration-token > response.json 
+
+# Ensure response.json is owned by the current user
+sudo chown $USER:$USER response.json
 
 RUNNER_TOKEN=$(jq -r '.token' response.json)
 echo "RUNNER_TOKEN: $RUNNER_TOKEN"
@@ -49,6 +52,9 @@ ghrunner-vm01
 self-hosted,Linux,X64,ghrunner-vm01
 _work
 EOF
+
+# Ensure correct ownership before installing the service
+sudo chown -R $USER:$USER "$HOME/actions-runner"
 
 # Install and start the service
 sudo ./svc.sh install
