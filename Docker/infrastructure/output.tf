2a6458a89b9848086fb6b02c80e278be8474c4a0
tsrlearning <info@tsrlearning.io>
2024-06-29 15:55:58 -0500

    deploy resume app

--- Docker/infrastructure/output.tf
+++ Docker/infrastructure/output.tf
@@ -1,6 +1,6 @@
-# output "vm_1" {
-#   value = azurerm_public_ip.vm_1.ip_address
-# }
+output "vm_1" {
+  value = azurerm_public_ip.vm_1.ip_address
+}
 
 output "vm_2" {
   value = azurerm_public_ip.vm_2.ip_address
